5bbf00fb42b61adb189f5fa18d1ff9a8d1334094
Anuruddha <thecodexbit@gmail.com>
2026-08-05 22:39:19 +0530

    Time: 3 ms (95.23%), Space: 45 MB (66.74%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,28 +1,18 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         List<Integer> ls = new ArrayList<>();
-        HashMap<Integer , Integer> map1 = new HashMap<>();
-            for(int i=0 ; i<nums1.length ; i++){
-                if(map1.containsKey(nums1[i])){
-                    map1.put(nums1[i] , map1.get(nums1[i])+1);
-                }else{
-                    map1.put(nums1[i] , 1);
-                }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums1.length; i++){
+            if(map.containsKey(nums1[i])){
+                map.put(nums1[i], map.get(nums1[i]) + 1);
+            }else{
+                map.put(nums1[i], 1);
             }
-        HashMap<Integer , Integer> map2 = new HashMap<>();
-            for(int i=0 ; i<nums2.length ; i++){
-                if(map2.containsKey(nums2[i])){
-                    map2.put(nums2[i] , map2.get(nums2[i])+1);
-                }else{
-                    map2.put(nums2[i] , 1);
-                }
-            }
-        for(int key : map1.keySet()){
-            if(map2.containsKey(key)){
-                int freq = Math.min(map1.get(key) , map2.get(key));
-                while(freq-- > 0){
-                    ls.add(key);
-                }
+        }
+        for(int i = 0; i < nums2.length; i++){
+            if(map.containsKey(nums2[i]) && map.get(nums2[i]) > 0){
+                ls.add(nums2[i]);
+                map.put(nums2[i], map.get(nums2[i]) - 1);
             }
         }
         int[] ans = new int[ls.size()];
