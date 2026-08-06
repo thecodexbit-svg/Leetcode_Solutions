2aa0f5e100e61d45ed29581bb9a99feb75745af7
Anuruddha <thecodexbit@gmail.com>
2026-08-06 16:41:38 +0530

    Time: 0 ms (100%), Space: 46.5 MB (32.56%) - LeetHub

---
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int[] preMax = new int[arr.length];
+        preMax[0] = arr[0];
+        for(int i=1 ; i<arr.length ; i++){
+            preMax[i] = Math.max(preMax[i-1] , arr[i]);
+        }
+        int[] suffMin = new int[arr.length];
+        suffMin[arr.length-1] = arr[arr.length-1];
+        for(int i=arr.length-2 ; i>=0 ; i--){
+            suffMin[i] = Math.min(suffMin[i+1] , arr[i]);
+        }
+        int ans = 1;
+        for(int i=0 ; i<arr.length-1 ; i++){
+            if(preMax[i]<=suffMin[i+1]){
+                ans+=1;
+            }
+        }
+        return ans;
+    }
+}
