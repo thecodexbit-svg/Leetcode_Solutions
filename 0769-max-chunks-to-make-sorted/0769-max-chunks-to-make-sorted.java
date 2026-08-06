3f27428961028a27642e643d7359662f4ea3f4e6
Anuruddha <thecodexbit@gmail.com>
2026-08-06 16:10:56 +0530

    Time: 0 ms (100%), Space: 42.8 MB (14.75%) - LeetHub

---
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int ans = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i=0 ; i<arr.length ; i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+            if(max == i){
+                ans += 1;
+                max = Integer.MIN_VALUE;
+            }
+        }
+        return ans;
+
+
+    }
+}
