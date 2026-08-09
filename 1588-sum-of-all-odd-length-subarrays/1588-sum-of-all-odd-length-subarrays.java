c217c31c6a8ce04a655f64726eafba1d7a71400e
Anuruddha <thecodexbit@gmail.com>
2026-08-10 00:08:21 +0530

    Time: 0 ms (100%), Space: 43.2 MB (24.55%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -3,7 +3,7 @@ class Solution {
         int n = arr.length;
         long totalSum = 0;
         for(int i=0 ; i<n ; i++){
-            long count = ((long)(i+1)*(n-i)+1)/2;
+            long count = ((long)(i+1)*(n-i)+1)/2; // formula of contribution of that element in every odd length subarray.
             totalSum += arr[i] * count;
         }
         return (int)totalSum;
