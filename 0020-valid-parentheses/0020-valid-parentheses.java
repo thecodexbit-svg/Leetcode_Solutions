964e261be12b097050bda0bf46bcc34e3bb2516b
Anuruddha <thecodexbit@gmail.com>
2026-08-11 00:15:26 +0530

    Time: 5 ms (13.19%), Space: 44 MB (5.69%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
+        ArrayDeque<Character> st = new ArrayDeque<>();
         for(int i=0 ; i<s.length() ; i++){
             if(s.charAt(i) == '[' ||
                 s.charAt(i) == '{' ||
