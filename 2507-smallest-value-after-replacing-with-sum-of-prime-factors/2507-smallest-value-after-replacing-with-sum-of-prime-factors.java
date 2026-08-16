94b1f7a64667733004dbc0654ae19743fbab2306
Anuruddha <thecodexbit@gmail.com>
2026-08-16 19:48:47 +0530

    Time: 0 ms (100%), Space: 42.4 MB (34.08%) - LeetHub

--- 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.java
+++ 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.java
@@ -1,40 +1,61 @@
 class Solution {
     public int smallestValue(int n) {
-        int sum = 0;
-        for(int i = 2 ; i*i<= n ; i++){
-            if(n %i ==0){
-                while(n %i ==0){
-                    sum += i;
-                    n /= i;
-                }
-            }
-        }
-        if(n > 1) {
-            sum += n;
-        }
+        // int sum = 0;
+        // for(int i = 2 ; i*i<= n ; i++){
+        //     if(n %i ==0){
+        //         while(n %i ==0){
+        //             sum += i;
+        //             n /= i;
+        //         }
+        //     }
+        // }
+        // if(n > 1) {
+        //     sum += n;
+        // }
         
-        return helper(sum);
-    }
-    private int helper(int sum){
-        int preSum = sum;
-        int s = 0;
-        List<Integer> tempfact = new ArrayList<>();
-        for(int i = 2 ; i*i<= sum ; i++){
-            if(sum %i ==0){
-                while(sum %i ==0){
-                    s += i;
-                    sum /= i;
+        // return helper(sum);
+
+        while(true){
+            int preSum = n;
+            int sum = 0;
+            for(int i=2 ; i*i<=n ; i++){
+                if(n % i == 0){
+                    while(n % i == 0){
+                        sum += i;
+                        n /= i;
+                    }
                 }
             }
-        }
-        if(sum > 1){
-            s += sum;
+            if(n > 1){
+                sum += n;
+            }
+            if(preSum == sum){
+                return preSum;
+            }
+            n = sum;
         }
 
-        if(preSum == s){
-            return s;
-        }
-
-        return helper(s);
     }
+    // private int helper(int sum){
+    //     int preSum = sum;
+    //     int s = 0;
+    //     List<Integer> tempfact = new ArrayList<>();
+    //     for(int i = 2 ; i*i<= sum ; i++){
+    //         if(sum %i ==0){
+    //             while(sum %i ==0){
+    //                 s += i;
+    //                 sum /= i;
+    //             }
+    //         }
+    //     }
+    //     if(sum > 1){
+    //         s += sum;
+    //     }
+
+    //     if(preSum == s){ //BASE CASE
+    //         return s;
+    //     }
+
+    //     return helper(s);
+    // }
 }
