9923a648c253a09edaaff93ae4dff4957bf543cb
Anuruddha <thecodexbit@gmail.com>
2026-08-16 19:42:27 +0530

    Time: 1 ms (47.09%), Space: 41.9 MB (92.83%) - LeetHub

---
+++ 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int smallestValue(int n) {
+        int sum = 0;
+        for(int i = 2 ; i*i<= n ; i++){
+            if(n %i ==0){
+                while(n %i ==0){
+                    sum += i;
+                    n /= i;
+                }
+            }
+        }
+        if(n > 1) {
+            sum += n;
+        }
+        
+        return helper(sum);
+    }
+    private int helper(int sum){
+        int preSum = sum;
+        int s = 0;
+        List<Integer> tempfact = new ArrayList<>();
+        for(int i = 2 ; i*i<= sum ; i++){
+            if(sum %i ==0){
+                while(sum %i ==0){
+                    s += i;
+                    sum /= i;
+                }
+            }
+        }
+        if(sum > 1){
+            s += sum;
+        }
+
+        if(preSum == s){
+            return s;
+        }
+
+        return helper(s);
+    }
+}
