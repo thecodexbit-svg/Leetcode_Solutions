32d4b903d175443f8ae43dd4ab166b39e7259054
Anuruddha <thecodexbit@gmail.com>
2026-08-24 11:58:49 +0530

    Time: 11 ms (65.73%), Space: 50.3 MB (19.97%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int min = 0;
+        int max = 0;
+        for(int val : weights){
+            min = Math.max(min , val);
+            max += val;
+        }
+
+        while(min < max){
+            int cap = min + (max-min)/2;
+            int d = checking(weights , cap);
+            if (d <= days) {
+                max = cap;
+            } else {
+                min = cap + 1;
+            }
+        }
+        return min;
+    }
+    private int checking(int[] weights , int cap){
+        int days = 1;
+        int sum = 0;
+        for (int weight : weights) {
+            if (sum + weight > cap) {
+                days++;
+                sum = 0;
+            }
+            sum += weight;
+        }
+        return days;
+    }
+}
