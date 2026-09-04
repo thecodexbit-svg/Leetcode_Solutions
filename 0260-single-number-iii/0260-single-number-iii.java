ef815b10116cf53b685fa74862410bb0fbd50c4b
Anuruddha <thecodexbit@gmail.com>
2026-09-04 11:27:52 +0530

    Time: 1 ms (100%), Space: 48 MB (58.88%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        if(nums.length <= 2) return nums;
+        int ab = 0;
+        for(int num : nums){
+            ab ^= num;
+        }
+        int rightSetBit = ab & -ab;
+        int a = 0;
+        for(int num : nums){
+            if((num & rightSetBit) != 0){
+                a ^= num;
+            }
+        }
+        return new int[]{a , ab^a};
+    }
+}
