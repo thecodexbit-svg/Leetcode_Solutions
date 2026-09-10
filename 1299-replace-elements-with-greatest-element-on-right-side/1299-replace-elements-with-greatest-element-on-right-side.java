aaf87ae66c55a904f4639e1cd82dcaae03ddcefa
Anuruddha <thecodexbit@gmail.com>
2026-09-10 10:43:51 +0530

    Time: 3 ms (68.76%), Space: 49 MB (79.41%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
-        int []sufMax = new int[arr.length];
-        sufMax[arr.length-1] = arr[arr.length-1];
+        // suffix max
         for(int i=arr.length-2 ; i>=0 ; i--){
             arr[i] = Math.max(arr[i] , arr[i+1]);
         }
+        // shifting by one index
         for(int i=0 ; i<arr.length-1 ; i++){
             arr[i] = arr[i+1];
         }
